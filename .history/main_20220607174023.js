c88f43f85be3e34bb7847dd29f3eb6971976f6b3
santig20 <santiagogrenat@gmail.com>
2022-06-07 19:19:45 -0300

    first commit

---
+++ .history/main_20220607174023.js
@@ -0,0 +1,123 @@
+
+//la primera forma que 
+ /*let ingrediente = prompt ("ingresa el ingrediente");{
+     const guardaringrediente = ingrediente;
+     console.log ("ingrediente:" + guardaringrediente);
+
+     if(ingrediente.length %2 ==0){
+         console.log( "el ingrediente es par")
+     } else {
+         console.log ("el ingrediente es impar")
+     }
+    
+ }
+
+ let ingrediente1 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente1 = ingrediente1;
+    console.log ("ingrediente:" + guardaringrediente1);
+
+    if(ingrediente1.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+
+let ingrediente2 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente2 = ingrediente2;
+    console.log ("ingrediente:" + guardaringrediente2);
+
+    if(ingrediente2.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+ 
+let ingrediente3 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente3 = ingrediente3;
+    console.log ("ingrediente:" + guardaringrediente3);
+
+    if(ingrediente3.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+
+let ingrediente4 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente4 = ingrediente4;
+    console.log ("ingrediente:" + guardaringrediente4);
+
+    if(ingrediente4.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+ 
+let ingrediente5 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente5 = ingrediente5;
+    console.log ("ingrediente:" + guardaringrediente5);
+
+    if(ingrediente5.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+
+let ingrediente6 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente6 = ingrediente6;
+    console.log ("ingrediente:" + guardaringrediente6);
+
+    if(ingrediente6.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+
+let ingrediente7 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente7 = ingrediente7;
+    console.log ("ingrediente:" + guardaringrediente7);
+
+    if(ingrediente7.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+
+let ingrediente8 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente8 = ingrediente8;
+    console.log ("ingrediente:" + guardaringrediente8);
+
+    if(ingrediente8.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}
+
+let ingrediente9 = prompt ("ingresa el ingrediente");{
+    const guardaringrediente9 = ingrediente9;
+    console.log ("ingrediente:" + guardaringrediente9);
+
+    if(ingrediente9.length %2 ==0){
+        console.log( "el ingrediente es par")
+    } else {
+        console.log ("el ingrediente es impar")
+    }
+   
+}*/
+
+
